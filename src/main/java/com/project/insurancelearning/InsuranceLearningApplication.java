b8e7856c63cb93442ea07ec231122a8332cdf5bc
KhaiCX <101654542+KhaiCX@users.noreply.github.com>
2023-11-18 21:11:58 +0700

    Initial insurance learning online project

---
+++ src/main/java/com/project/insurancelearning/InsuranceLearningApplication.java
@@ -0,0 +1,13 @@
+package com.project.insurancelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/insurancelearning/InsuranceLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.insurancelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
